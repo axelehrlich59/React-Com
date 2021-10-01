ed138d3f2a79593159102b9a78630f13c0a5acec
axel <axelehrlich8@gmail.com>
2021-10-01 10:44:42 +0200

    first commit

---
+++ src/sidebar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+
+
+const Sidebar = () => {
+  return (
+    <div
+      style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
+    >
+      <CDBSidebar textColor="#fff" backgroundColor="#333">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a
+            href="/"
+            className="text-decoration-none"
+            style={{ color: 'inherit' }}
+          >
+            Sidebar
+          </a>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/Commercial" activeClassName="activeClicked">
+                
+              <CDBSidebarMenuItem icon="columns">Commercial</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Marcom" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Macrom</CDBSidebarMenuItem>
+            </NavLink>
+            
+
+            
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            style={{
+              padding: '20px 5px',
+            }}
+          >
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
+};
+
+export default Sidebar;
