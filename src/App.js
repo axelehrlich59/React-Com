37e587f19ff0f7d6bb4c990f9be599deff915c00
axel <axelehrlich8@gmail.com>
2021-10-02 12:30:16 +0200

    add section bloc 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './sidebar';
 import marcom from './marcom';
-import commercial from "./commercial";
+import commercialBloc1 from "./commercial";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App" style={{display:'flex'}}>
+    <div className="App" style={{display:'flex', width:'100%'}}>
       <Router>
         <Sidebar/>
-          <Route path="/" exact component={commercial}/>
+          <Route path="/" exact component={commercialBloc1}/>
           <Route path="/marcom" exact component={marcom}/>
       </Router>
     </div>
--- src/commercial.js
+++ src/commercial.js
@@ -1,7 +1,50 @@
 import React from "react";
 
-const commercial = () => {
-    return <div style={{display: 'flex'}}><h1>Hello commercial</h1></div>;
+const commercialBloc1 = () => {
+
+    return  <div style={{width:'100%', backgroundColor:'whitesmoke'}}>    
+
+                <div className='section_com'>
+                    <p>section</p>
+                    <h5 className='section_h5'>Commercial</h5>
+                </div>
+
+                <div className='container_main_bloc1'>
+
+                        <div className='container_bloc1'>
+                    
+
+                                <div className='container_1'>
+                                    <label>
+                                        Objectif
+                                    </label>
+                                    <label>
+                                        1 000 000 $
+                                    </label>
+                                </div>
+                                <div className='container_2'>
+                                    <label>
+                                        Propales envoyées
+                                    </label>
+                                    <label>
+                                        70
+                                    </label>
+                                </div>
+                                <div className='container_3'>
+                                    <label>CA global</label>
+                                    <label>438 042 $</label>
+                                </div>
+                                <div className='container_4'>
+                                    <label>Rendez-vous</label> 
+                                    <label>118</label>      
+                                </div>
+
+                        </div>
+                </div>  
+       
+       </div>
+
+       ;
 }
 
-export default commercial;
+export default commercialBloc1;
