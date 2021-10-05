9de7edf5aa05ec7e3d00fa0065778e51fe1b048c
axel <axelehrlich8@gmail.com>
2021-10-05 14:49:39 +0200

    marcom bloc 1 icon

--- src/marcomBloc1.js
+++ src/marcomBloc1.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCreditCard, faHandPointRight, faUserFriends, faUserPlus} from '@fortawesome/free-solid-svg-icons';
 
 export default class marcomBloc1 extends React.Component{
 
@@ -16,28 +18,45 @@ export default class marcomBloc1 extends React.Component{
                 <div className='container_bloc_1'>
                     <div className='container_all_column'>
                         <div className='container_column1'>
-                            <div className='container_content1'>
+                            
+                                <div className='container_icon'> 
+                                    <FontAwesomeIcon className='icon_finger' icon={faHandPointRight} />
+                                </div>
+                                <div className='container_label'>
                                 <label className='container_content1_label1'>Actions</label><br />
                                 <label className='container_content1_label2'>130</label>
-                            </div>
+                                </div>
+                       
                         </div>
                         <div className='container_column2'>
-                            <div className='container_content2'>
-                                <label className='container_content2_label1'>Mises en contact</label> <br />
+                                <div className='container_icon'> 
+                                <FontAwesomeIcon className='user_gear' icon={faUserFriends} />
+                                </div>
+                                <div className='container_label'>
+                                <label className='container_content2_label1'>Mises en contact</label><br />
                                 <label className='container_content2_label2'>2519</label>
-                            </div>
+                                </div>
+                           
                         </div>
                         <div className='container_column3'>
-                            <div className='container_content3'>
-                                <label className='container_content3_label1'>Leads obtenus</label> <br />
-                                <label className='container_content3_label2'>171</label>
-                            </div>
+                                 <div className='container_icon'> 
+                                 <FontAwesomeIcon className='user_plus' icon={faUserPlus} />
+                                </div>
+                                <div className='container_label'>
+                                    <label className='container_content3_label1'>Leads obtenus</label> <br />
+                                    <label className='container_content3_label2'>171</label>
+                                </div>
+                            
                         </div>
                         <div className='container_column4'>
-                            <div className='container_content4'>
-                                <label className='container_content4_label1'>Coût / Lead</label> <br />
-                                <label className='container_content4_label2'>3,24€</label>
-                            </div>
+                                <div className='container_icon'> 
+                                <FontAwesomeIcon className='credit_card' icon={faCreditCard} />
+                                </div>
+                                <div className='container_label'>
+                                    <label className='container_content4_label1'>Coût / Lead</label> <br />
+                                    <label className='container_content4_label2'>3,24€</label>
+                                </div>
+                            
                         </div>
                     </div>
                 </div>
