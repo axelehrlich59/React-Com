491a2362983e886fdfccff39756899c2da8c13c8
axel <axelehrlich8@gmail.com>
2021-10-05 15:14:00 +0200

    project done

--- src/marcomBloc2.js
+++ src/marcomBloc2.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faHandPointUp } from '@fortawesome/free-solid-svg-icons';
    
  export default class marcomBloc2 extends React.Component {
      render() {
@@ -6,14 +8,28 @@ import React from "react";
             <div className='container_parent'>
               <div className='container_main_bloc2'>
                   <div className='container_bloc2'>
+
+                     
+
                       <div className='column1'>
-                          <label className='column1_label1'>Vues</label> <br/>
-                          <label className='column1_label2'>1282</label>
-                          </div>
+                            <div className='container_icon_bloc2'>
+                                <FontAwesomeIcon className='eye_icon' icon={faEye} />
+                            </div>
+
+                            <div className='container_label_bloc2'>
+                                <label className='column1_label1'>Vues</label> <br/>
+                                <label className='column1_label2'>1282</label>
+                            </div>
+                      </div>
 
                       <div className='column2'>
-                          <label className='column2_label1'>Interractions</label> <br />
-                          <label className='column2_label2'>54</label>
+                            <div className='container_icon_bloc2'>
+                            <FontAwesomeIcon className='hand_icon' icon={faHandPointUp} />
+                            </div>
+                            <div className='container_label_bloc2'>  
+                                <label className='column2_label1'>Interractions</label> <br />
+                                <label className='column2_label2'>54</label>
+                            </div>
                       </div>
                   </div>
               </div>
